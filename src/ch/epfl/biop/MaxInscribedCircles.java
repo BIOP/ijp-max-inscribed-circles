e1356eb61f1c9d0f646154e14b49918662014d75
Olivier Burri <olivier.burri@epfl.ch>
2016-07-26 17:28:01 +0200

    Added POM and plugin config file from mavenized project

--- src/ch/epfl/biop/MaxInscribedCircles.java
+++ src/ch/epfl/biop/MaxInscribedCircles.java
@@ -1,16 +1,22 @@
 package ch.epfl.biop;
 
-import fiji.process3d.SEDT;
+import java.awt.Point;
+import java.awt.Polygon;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+
+import fiji.process3d.EDT;
 import ij.IJ;
 import ij.ImagePlus;
 import ij.ImageStack;
 import ij.gui.OvalRoi;
 import ij.gui.Roi;
+import ij.plugin.filter.MaximumFinder;
 import ij.process.Blitter;
 import ij.process.ByteProcessor;
 import ij.process.ImageProcessor;
-
-import java.util.ArrayList;
 
 public class MaxInscribedCircles {
 
@@ -41,83 +47,150 @@ public class MaxInscribedCircles {
 			offsetY = sel.getBounds().y;
 			
 			if(isSelectionOnly) {	
-				iptmp = sel.getMask();
+				iptmp = sel.getMask().resize(sel.getPolygon().getBounds().width*2);
 			} else {
-				iptmp = imp.getProcessor().crop();
-				sel.setLocation(0, 0);
-				iptmp.fillOutside(sel);
+				iptmp = imp.getProcessor().resize(sel.getPolygon().getBounds().width*2);
 				sel.setLocation(offsetX, offsetY);
-
 			}
+			
 			ip = new ByteProcessor(iptmp.getWidth()+4, iptmp.getHeight()+4);
 			ip.copyBits(iptmp, 2,2, Blitter.ADD);
 			offsetX-=2;
 			offsetY-=2;
-			
 
 		// Otherwise use the whole image
 		} else {
-			ip = imp.getProcessor().duplicate();
+			ip = imp.getProcessor().resize(imp.getWidth()*2);
 			offsetX = 0;
 			offsetY = 0;
 		}
 		
 		// Prepare stack for Distance Transform
-		ImageStack tmpstk = new ImageStack(ip.getWidth(), ip.getHeight(), 1);
-		tmpstk.setProcessor(ip, 1);
+		ImageStack tmpstk = new ImageStack(ip.getWidth(), ip.getHeight());
 		
-		// This class handles the distance transform
-		SEDT sedt = new SEDT();
-		
-		double r;
-		
-		int width = ip.getWidth();
-		boolean done = false;
-		while (!done) {
-			//Find the position of the max;
-			float[] px = (float[]) sedt.compute(tmpstk).getProcessor().getPixels();
-			float themax = 0;
-			int   idx = 0;	
-			for(int i=0; i<px.length; i++) {
-				if (px[i] > themax) {
-					idx = i;
-					themax = px[i];
-				}
-			}
-	
-			// Get Diameter of circle
-			r = 2*themax;		
+		tmpstk.addSlice(ip);
 
-			if (r>minD) {
-				
-				IJ.showStatus("Finding Largest Inscribed Circles D="+r+"...");
-									
-				// Get coordinates of circle
-				double posx = (idx%width)-r/2+0.5;
-				double posy = (idx/width)-r/2+0.5;
-				
-				// Create Roi
-				Roi circ = new OvalRoi(posx,posy, r, r);
-				circ.setStrokeWidth(2);
-				
-				// Remove from mask for next iteration
-				ip.fill(circ);
-	
-				// Add to Roi Manager on original image
-				circ.setLocation(posx+offsetX, posy+offsetY);
-				allrois.add(circ);
-				
-				// If the minD was 0, the user only gets the largest circle, singular
-				if (minD==0) {
-					done = true;
+		// This class handles the distance transform
+		EDT edt = new EDT();
+		double posx, posy, r;
+		
+		boolean done = false;
+		
+		while(!done) {
+			// Try finding all maxima first then picking the ones with no neighbors closer than the radius
+			MaximumFinder mf = new MaximumFinder();
+			
+			ImageProcessor dist_map_ip = edt.compute(tmpstk).getProcessor();
+			Polygon points = mf.getMaxima(dist_map_ip, 1, false);
+			
+			
+			// Sort them and only process those that have the maximum size above the minD
+			ArrayList<Point> hits = getSortedPoints(points, minD, dist_map_ip);
+			
+			// Make this list an ArrayList so we can easily add or pop elements
+			// ArrayList<Point> hits = new ArrayList<Point>();
+			//for(int i=0;i<points.npoints; i++) {
+			//	hits.add(new Point(points.xpoints[i], points.ypoints[i]));
+			//}
+			
+			done=true;
+			for (int k =0; k<hits.size(); k++) {
+				boolean is_draw = true;
+				Point p = hits.get(k);
+				r = dist_map_ip.getInterpolatedValue(p.x, p.y);
+
+				ArrayList<Point> neigh = findNeighbors(p, hits, dist_map_ip);
+				//IJ.log("Point "+p.toString()+" r:"+r+"has "+neigh.size()+" neighbors");
+				// It's not about the NEAREST point. it's IF THERE ARE points whose coordinates are within the radius
+				if (neigh.size() > 1) {
+					// Draw only if it's the largest.
+					for( Point pp : neigh) {
+						double r2 = dist_map_ip.getInterpolatedValue(pp.x, pp.y);
+						if (r < r2) {
+							is_draw = false;
+							break;
+						}
+					}				
 				}
 				
-			} else {
-				done = true;
+				if(is_draw) {
+					// Get coordinates of circle
+					posx = (p.x)-r+0.5;
+					posy = (p.y)-r+0.5;
+								
+					// Create Roi for mask
+					Roi circ = new OvalRoi(posx,posy, r*2, r*2);
+					// Fill ip to recalculate EDM based on this
+					ip.fill(circ);
+
+					// Original size Roi
+					posx = ((p.x)-r)/2+0.5;
+					posy = ((p.y)-r)/2+0.5;
+					circ = new OvalRoi(posx,posy, r, r);
+					circ.setStrokeWidth(1);
+
+					circ.setLocation(posx+offsetX, posy+offsetY);
+					allrois.add(circ);
+					done=false;
+
+				}
 			}
 		}
+		
 		IJ.showStatus("Done...");
-
+	
 		return allrois;
+	
+	}
+	
+	private static ArrayList<Point> getSortedPoints(Polygon points, double minD, final ImageProcessor ip) {
+		ArrayList<Point> hits = new ArrayList<Point>(points.npoints);
+		List<Point> tmpList = new ArrayList<Point>(points.npoints);
+		
+		for(int i=0; i<points.npoints; i++) {
+			double r = ip.getInterpolatedValue(points.xpoints[i], points.ypoints[i]);
+			if(r > minD) {
+				hits.add(new Point(points.xpoints[i], points.ypoints[i]));
+			}
+		}
+		
+		Collections.sort(hits, new Comparator<Point>() {
+		    @Override
+		    public int compare(Point p1, Point p2) {
+		        return new Double(ip.getInterpolatedValue(p2.x, p2.y)).compareTo(new Double(ip.getInterpolatedValue(p1.x, p1.y)));
+		    }
+		});
+		
+		int ind = 0;
+		for(int i=1; i< hits.size(); i++) {
+			Point p = hits.get(i);
+			if( ip.getInterpolatedValue(p.x, p.y) < ip.getInterpolatedValue(hits.get(0).x, hits.get(0).y) ) {
+				// We should remove all objects from this index onwards
+				ind = i;
+				break;
+			}
+		}
+		tmpList.addAll(hits);
+		if (ind > 0) {
+			tmpList = hits.subList(0, ind);
+		}
+		ArrayList<Point> finalList = new ArrayList<Point>();
+		finalList.addAll(tmpList);
+		return hits;
+	}
+
+	public static ArrayList<Point> findNeighbors(Point p, ArrayList<Point> hits, ImageProcessor ip) {
+		ArrayList<Point> neighbors = new ArrayList<Point>();
+		double r = ip.getInterpolatedValue(p.x, p.y);
+
+		for(Point k : hits) {
+			double dist = p.distance(k);
+			double r2 = ip.getInterpolatedValue(k.x, k.y);
+			if (dist < (r+r2)) {
+				neighbors.add(new Point(k));
+			}
+		}		
+		return neighbors;
+			
 	}
 }
