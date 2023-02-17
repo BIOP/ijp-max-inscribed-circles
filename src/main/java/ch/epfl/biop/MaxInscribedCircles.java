6d2d4e338a76c49c94a7528ccb06fe8752612cd4
oburri <olivier.burri@gmail.com>
2023-02-17 13:58:56 +0100

    major overhaul with builder pattern

--- src/main/java/ch/epfl/biop/MaxInscribedCircles.java
+++ src/main/java/ch/epfl/biop/MaxInscribedCircles.java
@@ -31,28 +31,100 @@ import ij.IJ;
 import ij.ImagePlus;
 import ij.ImageStack;
 import ij.gui.OvalRoi;
+import ij.gui.Overlay;
 import ij.gui.Roi;
 import ij.plugin.filter.MaximumFinder;
 import ij.process.ByteProcessor;
 import ij.process.ImageProcessor;
 import java.awt.Polygon;
 import java.awt.geom.Point2D.Double;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class MaxInscribedCircles {
-	public MaxInscribedCircles() {
+
+	private ImagePlus imp;
+	private double minimumDiameter;
+	private double closenessTolerance;
+	private double minimumSimilarity;
+	private boolean getSpine;
+	private boolean useSelectionOnly;
+	private boolean appendPositionToName;
+
+	MaxInscribedCircles() {
 	}
 
-	public static ArrayList<Roi> findCircles(ImagePlus imp, double minD, boolean isSelectionOnly) {
+	public static Builder builder(ImagePlus imp) {
+		return new Builder(imp);
+	}
+
+	public List<Roi> findCircles() {
+		Overlay finalOverlay = new Overlay();
+
+		int nSlices = imp.getStackSize();
+
+		// Process stack only if it's not "selectiononly, otherwise process current slice only
+		int start = useSelectionOnly ? imp.getCurrentSlice() : 1;
+		int end = useSelectionOnly ? imp.getCurrentSlice() : nSlices;
+
+
+		for (int i = start; i <= end; i++) {
+			IJ.log("Processing Slice " + i + " of " + nSlices);
+			ImagePlus tmpImp = new ImagePlus(imp.getTitle() + " - Slice " + i, imp.getStack().getProcessor(i));
+			List<Roi> circles = MaxInscribedCircles.findCircles(tmpImp, minimumDiameter, useSelectionOnly);
+
+			// Add the position of the stack
+			for (Roi r : circles) {
+				r.setPosition(i);
+				if (appendPositionToName) r.setName(r.getName() + "-P_" + i);
+				finalOverlay.add(r);
+			}
+
+			// Display a message if no circle was found
+			if (circles.size() == 0) {
+				IJ.log("No circles found, consider decreasing 'Minimum Circle Diameter'.");
+			}
+
+			// Display a message if no circle was found
+			if (getSpine && circles.size() == 1) {
+				IJ.log("A single circle was found. Spine cannot be computed, consider decreasing 'Minimum Circle Diameter'.");
+			}
+
+			// Only get spine if checkbox is ticked and there is at least 2 circles
+			if (getSpine && circles.size() > 1) {
+				// Define the parameters
+				CirclesBasedSpine sbs = (new CirclesBasedSpine.Settings(tmpImp)).circles(circles).closenessTolerance(closenessTolerance).minSimilarity(minimumSimilarity).showCircles(false).build();
+				// Get the spine
+				Roi spine = sbs.getSpine();
+				// If one is found rename and add it to the Roi Manager
+				if (spine != null) {
+					spine.setPosition(i);
+					if (appendPositionToName) spine.setName(spine.getName() + "-P_" + i);
+					finalOverlay.add(spine);
+
+					// Get the overlay and add it back as well
+					Overlay ov = tmpImp.getOverlay();
+					for (Roi roi : ov) {
+						roi.setPosition(i);
+						finalOverlay.add(roi);
+					}
+				} else {
+					IJ.log("No spine found");
+				}
+			}
+		}
+		//imp.setOverlay(finalOverlay);
+
+		// Convert the overlay to a list
+		return Arrays.stream(finalOverlay.toArray()).collect(Collectors.toList());
+	}
+
+	public static List<Roi> findCircles(ImagePlus imp, double minD, boolean isSelectionOnly) {
 		IJ.showStatus("Finding Largest Inscribed Circles based on Distance Map...");
-		ArrayList<Roi> allrois = new ArrayList<Roi>();
+		List<Roi> allrois = new ArrayList<>();
 		Roi sel = imp.getRoi();
-		Object ip;
+
+		ImageProcessor ip;
 		int offsetX;
 		int offsetY;
 		if (sel != null && sel.isArea()) {
@@ -67,7 +139,7 @@ public class MaxInscribedCircles {
 			}
 
 			ip = new ByteProcessor(iptmp.getWidth() + 4, iptmp.getHeight() + 4);
-			((ImageProcessor)ip).copyBits(iptmp, 2, 2, 3);
+			ip.copyBits(iptmp, 2, 2, 3);
 			--offsetX;
 			--offsetY;
 		} else {
@@ -76,28 +148,26 @@ public class MaxInscribedCircles {
 			offsetY = 0;
 		}
 
-		ImageStack tmpstk = new ImageStack(((ImageProcessor)ip).getWidth(), ((ImageProcessor)ip).getHeight());
-		tmpstk.addSlice((ImageProcessor)ip);
+		ImageStack tmpstk = new ImageStack(ip.getWidth(), ip.getHeight());
+		tmpstk.addSlice(ip);
 		EDT edt = new EDT();
 		boolean done = false;
 
-		while(!done) {
+		while (!done) {
 			MaximumFinder mf = new MaximumFinder();
 			ImageProcessor dist_map_ip = edt.compute(tmpstk).getProcessor();
 			Polygon points = mf.getMaxima(dist_map_ip, 1.0D, false);
 			ArrayList<Double> hits = getSortedPoints(points, minD, dist_map_ip);
 			done = true;
 
-			for(int k = 0; k < hits.size(); ++k) {
+			for (int k = 0; k < hits.size(); ++k) {
 				boolean is_draw = true;
-				Double p = (Double)hits.get(k);
+				Double p = hits.get(k);
 				double r = dist_map_ip.getInterpolatedValue(p.x, p.y);
 				ArrayList<Double> neigh = findNeighbors(p, hits, dist_map_ip);
 				if (neigh.size() > 1) {
-					Iterator<Double> var27 = neigh.iterator();
 
-					while(var27.hasNext()) {
-						Double pp = (Double)var27.next();
+					for (Double pp : neigh) {
 						double r2 = dist_map_ip.getInterpolatedValue(pp.x, pp.y);
 						if (r < r2) {
 							is_draw = false;
@@ -110,12 +180,13 @@ public class MaxInscribedCircles {
 					double posx = p.x - r;
 					double posy = p.y - r;
 					Roi circ = new OvalRoi(posx, posy, r * 2.0D, r * 2.0D);
-					((ImageProcessor)ip).fill(circ);
+					ip.fill(circ);
 					posx = (p.x - r) / 2.0D;
 					posy = (p.y - r) / 2.0D;
 					circ = new OvalRoi(posx, posy, r, r);
+					circ.setName(String.format("Circle-r_%.3f", r));
 					circ.setStrokeWidth(1.0F);
-					circ.setLocation(posx + (double)offsetX, posy + (double)offsetY);
+					circ.setLocation(posx + (double) offsetX, posy + (double) offsetY);
 					allrois.add(circ);
 					done = false;
 				}
@@ -131,49 +202,41 @@ public class MaxInscribedCircles {
 	}
 
 	private static ArrayList<Double> getSortedPoints(Polygon points, double minD, final ImageProcessor ip) {
-		ArrayList<Double> hits = new ArrayList<Double>(points.npoints);
-		List<Double> tmpList = new ArrayList<Double>(points.npoints);
+		ArrayList<Double> hits = new ArrayList<>(points.npoints);
+		List<Double> tmpList = new ArrayList<>(points.npoints);
 
 		int ind;
-		for(ind = 0; ind < points.npoints; ++ind) {
-			double r = ip.getInterpolatedValue((double)points.xpoints[ind], (double)points.ypoints[ind]);
+		for (ind = 0; ind < points.npoints; ++ind) {
+			double r = ip.getInterpolatedValue( points.xpoints[ind], points.ypoints[ind]);
 			if (r > minD) {
-				hits.add(new Double((double)points.xpoints[ind], (double)points.ypoints[ind]));
+				hits.add(new Double( points.xpoints[ind], points.ypoints[ind]));
 			}
 		}
 
-		Collections.sort(hits, new Comparator<Double>() {
-			public int compare(Double p1, Double p2) {
-				return (new java.lang.Double(ip.getInterpolatedValue(p2.x, p2.y))).compareTo(new java.lang.Double(ip.getInterpolatedValue(p1.x, p1.y)));
-			}
-		});
+		hits.sort((p1, p2) -> java.lang.Double.compare(ip.getInterpolatedValue(p2.x, p2.y), ip.getInterpolatedValue(p1.x, p1.y)));
 		ind = 0;
 
-		for(int i = 1; i < hits.size(); ++i) {
-			Double p = (Double)hits.get(i);
-			if (ip.getInterpolatedValue(p.x, p.y) < ip.getInterpolatedValue(((Double)hits.get(0)).x, ((Double)hits.get(0)).y)) {
+		for (int i = 1; i < hits.size(); ++i) {
+			Double p = hits.get(i);
+			if (ip.getInterpolatedValue(p.x, p.y) < ip.getInterpolatedValue(hits.get(0).x, hits.get(0).y)) {
 				ind = i;
 				break;
 			}
 		}
 
-		((List<Double>)tmpList).addAll(hits);
+		tmpList.addAll(hits);
 		if (ind > 0) {
 			tmpList = hits.subList(0, ind);
 		}
 
-		ArrayList<Double> finalList = new ArrayList<Double>();
-		finalList.addAll((Collection<Double>)tmpList);
-		return finalList;
+		return new ArrayList<>(tmpList);
 	}
 
 	public static ArrayList<Double> findNeighbors(Double p, ArrayList<Double> hits, ImageProcessor ip) {
-		ArrayList<Double> neighbors = new ArrayList<Double>();
+		ArrayList<Double> neighbors = new ArrayList<>();
 		double r = ip.getInterpolatedValue(p.x, p.y);
-		Iterator<Double> var7 = hits.iterator();
 
-		while(var7.hasNext()) {
-			Double k = (Double)var7.next();
+		for (Double k : hits) {
 			double dist = p.distance(k);
 			double r2 = ip.getInterpolatedValue(k.x, k.y);
 			if (dist < r + r2) {
@@ -182,5 +245,106 @@ public class MaxInscribedCircles {
 		}
 
 		return neighbors;
+	}
+
+	public static class Builder {
+		private final ImagePlus imp;
+		private double minimumDiameter = 10;
+		private double closenessTolerance = 10;
+		private double minimumSimilarity = 0.5;
+		private boolean useSelectionOnly = false;
+		private boolean getSpine = false;
+
+		private boolean appendPositionToName = true;
+
+		Builder(ImagePlus imp) {
+			this.imp = imp;
+		}
+
+		/**
+		 * Set the minimum diameter in pixels of the circles to be found
+		 *
+		 * @param minimumDiameter smallest diameter in pixels of circles to be found. Can be very slow if < 10 pixels
+		 * @return this builder
+		 */
+		public Builder minimumDiameter(double minimumDiameter) {
+			if (minimumDiameter < 1) throw new IllegalArgumentException("Minimum diameter must be larger than 1 pixel");
+			this.minimumDiameter = minimumDiameter;
+			return this;
+		}
+
+		/**
+		 * Use the current selection of the current image for circle finding instead of a mask
+		 *
+		 * @param useSelectionOnly true to use the current selection instead of a mask
+		 * @return this builder
+		 */
+		public Builder useSelectionOnly(boolean useSelectionOnly) {
+			if (this.imp.getRoi() == null) {
+				IJ.log("No selection found, using mask instead");
+				useSelectionOnly = false;
+			}
+			this.useSelectionOnly = useSelectionOnly;
+			return this;
+		}
+
+		/**
+		 * Use the current selection of the current image for circle finding instead of a mask
+		 *
+		 * @param appendPositionToName Whether to append the position of the circle to the name of the circle (default: true)
+		 * @return this builder
+		 */
+		public Builder appendPositionToName(boolean appendPositionToName) {
+			this.appendPositionToName = appendPositionToName;
+			return this;
+		}
+
+		/**
+		 * Set the closeness tolerance in pixels of the circles to be found
+		 *
+		 * @param closenessTolerance The closeness tolerance in pixels of the circles to be found
+		 * @return this builder
+		 */
+		public Builder closenessTolerance(double closenessTolerance) {
+			if (closenessTolerance < 1)
+				throw new IllegalArgumentException("Closeness tolerance must be larger than 1 pixel");
+			this.closenessTolerance = closenessTolerance;
+			return this;
+		}
+
+		/**
+		 * Set the minimum similarity of the circles to be found
+		 *
+		 * @param minimumSimilarity minimum cosine similarity (0 to 1)
+		 * @return this builder
+		 */
+		public Builder minimumSimilarity(double minimumSimilarity) {
+			this.minimumSimilarity = minimumSimilarity;
+			return this;
+		}
+
+		/**
+		 * working from the largest circle, the closeness and similarity values, compute a spine going through the circles
+		 *
+		 * @param getSpine wether to return a circles based spine
+		 * @return this builder
+		 */
+		public Builder getSpine(boolean getSpine) {
+			this.getSpine = getSpine;
+			return this;
+		}
+
+		public MaxInscribedCircles build() {
+			MaxInscribedCircles mic = new MaxInscribedCircles();
+			mic.imp = this.imp;
+			mic.minimumDiameter = this.minimumDiameter;
+			mic.closenessTolerance = this.closenessTolerance;
+			mic.minimumSimilarity = this.minimumSimilarity;
+			mic.getSpine = this.getSpine;
+			mic.useSelectionOnly = this.useSelectionOnly;
+			mic.appendPositionToName = this.appendPositionToName;
+			return mic;
+
+		}
 	}
 }
